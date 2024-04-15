eaf1a5cd8ec5abe2e206f194d96e044001f09644
StarJwlly <StarJwlly>
2024-04-15 11:52:14 -0300

    feat(php): implement scrap function

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -14,17 +14,35 @@ class Scrapper {
    * Loads paper information from the HTML and returns the array with the data.
    */
   public function scrap(\DOMDocument $dom): array {
-    return [
-      new Paper(
-        123,
-        'The Nobel Prize in Physiology or Medicine 2023',
-        'Nobel Prize',
-        [
-          new Person('Katalin Karikó', 'Szeged University'),
-          new Person('Drew Weissman', 'University of Pennsylvania'),
-        ]
-      ),
-    ];
+    $sections = $dom->getElementsByTagName('section');
+    $result = [];
+
+    $finder = new \DomXPath($dom);
+    $classname="paper-card";
+    $nodes = $finder->query("//*[contains(@class, '$classname')]");
+
+    foreach($nodes as $node){
+      $authors = [];
+      foreach($node->firstChild->nextSibling->childNodes as $author){
+        if($author->nodeType == 1)
+          array_push($authors, new Person(
+            rtrim($author->nodeValue, ';'),
+            $author->attributes->getNamedItem('title')->value
+          ));
+      }
+      
+      array_push($result, new Paper(
+        $node->lastChild->lastChild->lastChild->nodeValue,
+        $node->firstChild->nodeValue,
+        $node->lastChild->firstChild->nodeValue,
+        $authors
+        ));
+    }
+
+
+    return [ $result ];
   }
+
+  
 
 }
