868776007960a6a56e8ddf2953e1e1ab22e8c42c
StarJwlly <>
2024-04-19 18:31:43 -0300

    chore(php) beautify code

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -8,44 +8,42 @@ use Chuva\Php\WebScrapping\Entity\Person;
 /**
  * Does the scrapping of a webpage.
  */
-class Scrapper
-{
+class Scrapper{
 
-    /**
-     * Loads paper information from the HTML and returns the array with the data.
-     */
-    public function scrap(\DOMDocument $dom): array
-    {
-        $sections = $dom->getElementsByTagName('section');
-        $result = [];
+  /**
+   * Loads paper information from the HTML and returns the array with the data.
+   */
+  public function scrap(\DOMDocument $dom): array
+  {
+    $sections = $dom->getElementsByTagName('section');
+    $result = []; 
 
-        $finder = new \DomXPath($dom);
-        $classname = "paper-card";
-        $nodes = $finder->query("//*[contains(@class, '$classname')]");
+    $finder = new \DomXPath($dom);
+    $classname = "paper-card";
+    $nodes = $finder->query("//*[contains(@class, '$classname')]");
 
-        foreach ($nodes as $node) {
-            $authors = [];
-            foreach ($node->firstChild->nextSibling->childNodes as $author) {
-                if ($author->nodeType == 1) {
-                    array_push(
-                        $authors, new Person(
-                            rtrim($author->nodeValue, ';'),
-                            $author->attributes->getNamedItem('title')->value
-                        )
-                    );
-                }
-            }
-      
-            array_push(
-                $result, new Paper(
-                    $node->lastChild->lastChild->lastChild->nodeValue,
-                    $node->firstChild->nodeValue,
-                    $node->lastChild->firstChild->nodeValue,
-                    $authors
-                )
-            );
+    foreach ($nodes as $node) {
+      $authors = [];
+      foreach ($node->firstChild->nextSibling->childNodes as $author) {
+        if ($author->nodeType == 1) {
+          array_push(
+            $authors, new Person(
+              rtrim($author->nodeValue, ';'),
+              $author->attributes->getNamedItem('title')->value
+            )
+          );
         }
-
-        return [$result];
+      }
+      array_push(
+        $result, new Paper(
+          $node->lastChild->lastChild->lastChild->nodeValue,
+          $node->firstChild->nodeValue,
+          $node->lastChild->firstChild->nodeValue,
+          $authors
+        )
+      );
     }
+
+    return [$result];
+  }
 }
