95ef4f73d0559b5df3efcc93d339353b11cf0ebb
StarJwlly <>
2024-04-19 18:36:53 -0300

    chore(php) beautify code

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -26,7 +26,7 @@ class Main {
       'Institution 3', 'Author 4', 'Institution 4', 'Author 5', 'Institution 5', 'Author 6', 'Institution 6',
       'Author 7', 'Institution 7', 'Author 8', 'Institution 8', 'Author 9', 'Institution 9', 'Author 10',
       'Institution 10', 'Author 11', 'Institution 11', 'Author 12', 'Institution 12', 'Author 13', 'Institution 13',
-      'Author 14', 'Institution 14', 'Author 15', 'Institution 15', 'Author 16', 'Institution 16'
+      'Author 14', 'Institution 14', 'Author 15', 'Institution 15', 'Author 16', 'Institution 16',
     ];
     $writer->addRow(WriterEntityFactory::createRowFromArray($header));
 
--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -45,5 +45,5 @@ class Scrapper {
 
     return [$result];
   }
-  
+
 }
